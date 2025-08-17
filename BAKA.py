896d890871550f94bb11385781aea250862a2cdc
DannyMang <daniel.ung@sjsu.edu>
2025-08-16 21:16:04 -0700

    test2

---
+++ BAKA.py
@@ -0,0 +1,15 @@
+def not_so_good_code():
+    for (int i = 0; i < 10; i++){
+        print(i)
+    }
+
+not_so_good_code()
+
+
+def not_so_good_code_2():
+    for i in range(10):
+        print(i)
+
+not_so_good_code_2()
+
+
