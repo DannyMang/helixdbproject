ac1aa8907ffecc874c29657c771f528dcdd367fe
ehcaw <ryannguyenc@gmail.com>
2025-08-27 11:00:54 -0700

    start server script

---
+++ start_server.sh
@@ -0,0 +1,8 @@
+curl -sSL https://install.helix-db.com | bash
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
+source ~/.bashrc
+helix install
+helix update
+helix deploy --path /src/helix/helix_config
+
+docker compose up --build
